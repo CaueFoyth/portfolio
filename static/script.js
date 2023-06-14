c9ff5ef10b062b03e1a9a2cfdebc5c2f50d6f6bd
CaueFoyth <foythcaue@gmail.com>
2023-06-13 21:18:16 -0300

    Animações

--- static/script.js
+++ static/script.js
@@ -38,3 +38,23 @@ navItem.forEach(item => {
         }
     })
 })
+
+//Animando todos os atributos que contem o data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    
+    item.forEach(element => {
+        if(windowTop > element.offsetTop){
+            element.classList.add('animate')
+        } else{
+            element.classList.remove('animate')
+        }
+    })
+}
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
